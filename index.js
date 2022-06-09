7553b3915ac89fccf4018ab5b344c36cc38e7591
Jon Ellwood <jonellwood@gmail.com>
2022-06-09 18:47:01 -0400

    fixed movement issues

--- index.js
+++ index.js
@@ -1,35 +1,113 @@
 console.log('This project is based in part on a tutorial from ChrisCourses.com with artwork from itch.io')
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
+
+const canvas = document.querySelector('canvas')
+const c = canvas.getContext('2d')
+
 canvas.width = 1024
 canvas.height = 576
 
-ctx.fillStyle = 'white';
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+c.fillStyle = 'white'
+c.fillRect(0, 0, canvas.width, canvas.height)
 
-const image = new Image();
-image.src = './images/EllwoodCity.png';
-// lets add a player image
-const playerImage = new Image();
+const image = new Image()
+image.src = './images/EllwoodCity.png'
+
+const playerImage = new Image()
 playerImage.src = './images/playerDown.png'
-// draw image inside of the onload method so the image is loaded from assets before its called to be rendered
-image.onload = () => {
-    ctx.drawImage(image, -585, -450);
-    ctx.drawImage(
-        playerImage,
-        // image cropping args
-        0,
-        0,
-        playerImage.width / 4,
-        playerImage.height,
-        canvas.width / 2 - playerImage.width / 4 / 2, 
-        canvas.height / 2 - playerImage.height / 2,
-        playerImage.width / 4,
-        playerImage.height, 
-        );
+
+class Sprite {
+  constructor({ position, velocity, image }) {
+    this.position = position
+    this.image = image
+  }
+
+  draw() {
+    c.drawImage(this.image, this.position.x, this.position.y)
+  }
 }
 
+const background = new Sprite({
+  position: {
+    x: -585,
+    y: -450
+  },
+  image: image
+})
 
+const keys = {
+  w: {
+    pressed: false
+  },
+  a: {
+    pressed: false
+  },
+  s: {
+    pressed: false
+  },
+  d: {
+    pressed: false
+  }
+}
 
-console.log("Welcome to Ellwood City");
-// console.log(ctx);
+function animate() {
+  window.requestAnimationFrame(animate)
+  background.draw()
+  c.drawImage(
+    playerImage,
+    0,
+    0,
+    playerImage.width / 4,
+    playerImage.height,
+    canvas.width / 2 - playerImage.width / 4 / 2,
+    canvas.height / 2 - playerImage.height / 2,
+    playerImage.width / 4,
+    playerImage.height
+  )
+
+  if (keys.w.pressed && lastKey === 'w') background.position.y += 3
+  else if (keys.a.pressed && lastKey === 'a') background.position.x += 3
+  else if (keys.s.pressed && lastKey === 's') background.position.y -= 3
+  else if (keys.d.pressed && lastKey === 'd') background.position.x -= 3
+}
+animate()
+
+let lastKey = ''
+window.addEventListener('keydown', (e) => {
+  switch (e.key) {
+    case 'w':
+      keys.w.pressed = true
+      lastKey = 'w'
+      break
+    case 'a':
+      keys.a.pressed = true
+      lastKey = 'a'
+      break
+
+    case 's':
+      keys.s.pressed = true
+      lastKey = 's'
+      break
+
+    case 'd':
+      keys.d.pressed = true
+      lastKey = 'd'
+      break
+  }
+})
+
+window.addEventListener('keyup', (e) => {
+  switch (e.key) {
+    case 'w':
+      keys.w.pressed = false
+      break
+    case 'a':
+      keys.a.pressed = false
+      break
+    case 's':
+      keys.s.pressed = false
+      break
+    case 'd':
+      keys.d.pressed = false
+      break
+  }
+})
