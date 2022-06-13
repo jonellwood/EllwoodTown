445d2d6d44799f22f189190d1af10a20ccf14dc6
Jon Ellwood <jonellwood@gmail.com>
2022-06-13 11:53:59 -0400

    boundries

--- index.js
+++ index.js
@@ -1,56 +1,111 @@
-console.log('This project is based in part on a tutorial from ChrisCourses.com with artwork from itch.io')
+console.log(
+  "This project is based in part on a tutorial from ChrisCourses.com with artwork from itch.io"
+);
+// console.log(collisions);
 
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
 
-canvas.width = 1024
-canvas.height = 576
+canvas.width = 1024;
+canvas.height = 576;
+const collisionsMap = [];
+// 70 in for loop is based on the tile width of the map
+for (let i = 0; i < collisions.length; i += 70) {
+  collisionsMap.push(collisions.slice(i, 70 + i));
+}
+// console.log(collisionsMap);
 
-c.fillStyle = 'white'
-c.fillRect(0, 0, canvas.width, canvas.height)
-
-const image = new Image()
-image.src = './images/EllwoodCity.png'
-
-const playerImage = new Image()
-playerImage.src = './images/playerDown.png'
-class Sprite {
-  constructor({ position, velocity, image }) {
-    this.position = position
-    this.image = image
+class Boundary {
+  static width = 48;
+  static height = 48;
+  constructor({ position }) {
+    this.position = position;
+    this.width = 48;
+    this.height = 48;
   }
 
   draw() {
-    c.drawImage(this.image, this.position.x, this.position.y)
+    c.fillStyle = "red";
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+}
+
+const boundaries = [];
+// create const for offset to it can be called for player position and boundary position with the same constant offset
+const offset = {
+  x: -585,
+  y: -450,
+};
+
+collisionsMap.forEach((row, i) => {
+  row.forEach((symbol, j) => {
+    if (symbol === 1025)
+      boundaries.push(
+        new Boundary({
+          position: {
+            x: j * Boundary.width + offset.x,
+            y: i * Boundary.height + offset.y,
+          },
+        })
+      );
+  });
+});
+
+// console.log(boundaries);
+
+const image = new Image();
+image.src = "./images/EllwoodCity.png";
+
+const playerImage = new Image();
+playerImage.src = "./images/playerDown.png";
+class Sprite {
+  constructor({ position, velocity, image }) {
+    this.position = position;
+    this.image = image;
+  }
+
+  draw() {
+    c.drawImage(this.image, this.position.x, this.position.y);
   }
 }
 
 const background = new Sprite({
   position: {
-    x: -585,
-    y: -450
+    x: offset.x,
+    y: offset.y,
   },
-  image: image
-})
+  image: image,
+});
 
 const keys = {
   w: {
-    pressed: false
+    pressed: false,
   },
   a: {
-    pressed: false
+    pressed: false,
   },
   s: {
-    pressed: false
+    pressed: false,
   },
   d: {
-    pressed: false
-  }
-}
+    pressed: false,
+  },
+};
+
+// const testBoundary = new Boundary({
+//   x: 400,
+//   y: 400,
+// });
+
+const movables = [background, boundaries];
 
 function animate() {
-  window.requestAnimationFrame(animate)
-  background.draw()
+  window.requestAnimationFrame(animate);
+  background.draw();
+  boundaries.forEach((boundary) => {
+    boundary.draw();
+  });
+
   c.drawImage(
     playerImage,
     0,
@@ -61,52 +116,65 @@ function animate() {
     canvas.height / 2 - playerImage.height / 2,
     playerImage.width / 4,
     playerImage.height
-  )
+  );
 
-  if (keys.w.pressed && lastKey === 'w') background.position.y += 3
-  else if (keys.a.pressed && lastKey === 'a') background.position.x += 3
-  else if (keys.s.pressed && lastKey === 's') background.position.y -= 3
-  else if (keys.d.pressed && lastKey === 'd') background.position.x -= 3
+  if (keys.w.pressed && lastKey === "w") {
+    movables.forEach((movable) => {
+      movable.position.y += 3;
+    });
+  } else if (keys.a.pressed && lastKey === "a") {
+    movables.forEach((movable) => {
+      movable.position.x += 3;
+    });
+  } else if (keys.s.pressed && lastKey === "s") {
+    movables.forEach((movable) => {
+      movable.position.y -= 3;
+    });
+  } else if (keys.d.pressed && lastKey === "d") {
+    movables.forEach((movable) => {
+      movable.position.x -= 3;
+    });
+  }
 }
-animate()
+animate();
 
-let lastKey = ''
-window.addEventListener('keydown', (e) => {
+let lastKey = "";
+window.addEventListener("keydown", (e) => {
   switch (e.key) {
-    case 'w':
-      keys.w.pressed = true
-      lastKey = 'w'
-      break
-    case 'a':
-      keys.a.pressed = true
-      lastKey = 'a'
-      break
+    case "w":
+      keys.w.pressed = true;
+      lastKey = "w";
+      break;
+    case "a":
+      keys.a.pressed = true;
+      lastKey = "a";
+      break;
 
-    case 's':
-      keys.s.pressed = true
-      lastKey = 's'
-      break
+    case "s":
+      keys.s.pressed = true;
+      lastKey = "s";
+      break;
 
-    case 'd':
-      keys.d.pressed = true
-      lastKey = 'd'
-      break
+    case "d":
+      keys.d.pressed = true;
+      lastKey = "d";
+      break;
   }
-})
+});
 
-window.addEventListener('keyup', (e) => {
+window.addEventListener("keyup", (e) => {
   switch (e.key) {
-    case 'w':
-      keys.w.pressed = false
-      break
-    case 'a':
-      keys.a.pressed = false
-      break
-    case 's':
-      keys.s.pressed = false
-      break
-    case 'd':
-      keys.d.pressed = false
-      break
+    case "w":
+      keys.w.pressed = false;
+      break;
+    case "a":
+      keys.a.pressed = false;
+      break;
+    case "s":
+      keys.s.pressed = false;
+      break;
+    case "d":
+      keys.d.pressed = false;
+      break;
   }
-})
+});
