6d0f6fef5045c52df3c92eadc89ff74dec1fe1a7
Jon Ellwood <jonellwood@gmail.com>
2022-06-16 14:31:08 -0400

    collison detection working

--- index.js
+++ index.js
@@ -64,6 +64,12 @@ class Sprite {
     this.position = position;
     this.image = image;
     this.frames = frames;
+    this.image.onload = () => {
+      this.width = this.image.width / this.frames.max;
+      this.height = this.image.height;
+      // console.log(this.width);
+      // console.log(this.height);
+    };
   }
 
   draw() {
@@ -83,7 +89,8 @@ class Sprite {
 
 const player = new Sprite({
   position: {
-    x: canvas.width / 2 - 192 / 4 / 2, // 192 and 68 come from the dimensions of the sprite
+    // 192 and 68 come from size of sprite found in finder
+    x: canvas.width / 2 - 192 / 4 / 2,
     y: canvas.height / 2 - 68 / 2,
   },
   image: playerImage,
@@ -115,20 +122,31 @@ const keys = {
   },
 };
 
-// const testBoundary = new Boundary({
-//   x: 400,
-//   y: 400,
-// });
+const testBoundary = new Boundary({
+  position: {
+    x: 400,
+    y: 400,
+  },
+});
 
-const movables = [background, ...boundaries];
+const movables = [background, testBoundary];
 
 function animate() {
   window.requestAnimationFrame(animate);
   background.draw();
-  boundaries.forEach((boundary) => {
-    boundary.draw();
-  });
+  // boundaries.forEach((boundary) => {
+  //   boundary.draw();
+  // });
+  testBoundary.draw();
   player.draw();
+  if (
+    player.position.x + player.width >= testBoundary.position.x &&
+    player.position.x <= testBoundary.position.x + testBoundary.width &&
+    player.position.y <= testBoundary.position.y + testBoundary.height &&
+    player.position.y + player.height >= testBoundary.position.y
+  ) {
+    console.log("crash");
+  }
 
   if (keys.w.pressed && lastKey === "w") {
     movables.forEach((movable) => {
