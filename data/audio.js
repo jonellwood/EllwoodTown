bdcc7f84bb47422895742d501995fd614d38ffc7
Jon Ellwood <jonellwood@gmail.com>
2022-06-21 15:57:34 -0400

    audio volume adjusted

--- data/audio.js
+++ data/audio.js
@@ -2,13 +2,16 @@ const audio = {
   Map: new Howl({
     src: "./audio/map.wav",
     html5: true,
+    volume: 0.1,
   }),
   initBattle: new Howl({
     src: "./audio/initBattle.wav",
     html5: true,
+    volume: 0.1,
   }),
   battle: new Howl({
     src: "./audio/battle.mp3",
     html5: true,
+    volume: 0.1,
   }),
 };
